3e85bf3d49701ff450fb873d0f75fa05639fbf0b
gosqib <69024184+gosqib@users.noreply.github.com>
2023-09-27 17:24:07 -0400

    some session 1

--- csca08/assn/a1/tickets.py
+++ csca08/assn/a1/tickets.py
@@ -65,7 +65,7 @@ def get_month(ticket: str) -> str:
 
 # We provide the docstring for this function to help you get started.
 def get_day(ticket: str) -> str:
-    """Return the day of ticket 'ticket'.
+    """Return the day of ticket 'ticket' in YYYYMMDD format.
 
     >>> get_day('20230915YYZYEG12F')
     '15'
@@ -134,20 +134,6 @@ def get_seat(ticket: str) -> str:
     return ticket[SEAT: SEAT + 1]
 
 
-def get_seat(ticket: str) -> str:
-    """Return the seat type of ticket 'ticket'.
-
-    >>> get_seat('20230915YYZYEG12F')
-    'F'
-    >>> get_seat('20241215YYZYEG12F1236')
-    'F'
-    >>> get_seat('20230926DFGKER19D')
-    'D'
-    """
-
-    return ticket[SEAT: SEAT + 1]
-
-
 def get_ffn(ticket: str) -> str:
     """Return the frequent flyer number (FFN) of ticket 'ticket'.
     If there is no FFN, an empty string '""' is returned.
@@ -169,22 +155,164 @@ Task 2: Validating the Information (12 correctness marks)
 
 
 # We provide the docstring for this function to help you get started.
-# def is_valid_seat(ticket: str, first_row: int, last_row: int) -> bool:
-#     """Return True if and only if this ticket has a valid seat. That is,
-#     if the seat row is between 'first_row' and 'last_row', inclusive,
-#     and the seat is SA, SB, SC, SD, SE, or SF.
+def is_valid_seat(ticket: str, first_row: int, last_row: int) -> bool:
+    """Return True if and only if this ticket has a valid seat. That is,
+    if the seat row is between 'first_row' and 'last_row', inclusive,
+    and the seat is SA, SB, SC, SD, SE, or SF.
+    Precondition: 'ticket' is in valid format.
 
-#     Precondition: 'ticket' is in valid format.
+    >>> is_valid_seat('20230915YYZYEG12F1236', 1, 30)
+    True
+    >>> is_valid_seat('20230915YYZYEG42F1236', 1, 30)
+    False
+    >>> is_valid_seat('20230915YYZYEG21Q1236', 1, 30)
+    False
+    """
 
-#     >>> is_valid_seat('20230915YYZYEG12F1236', 1, 30)
-#     True
-#     >>> is_valid_seat('20230915YYZYEG42F1236', 1, 30)
-#     False
-#     >>> is_valid_seat('20230915YYZYEG21Q1236', 1, 30)
-#     False
-#     """
+    return (
+        first_row <= int(get_row(ticket)) <= last_row and
+        get_seat(ticket) in (SA, SB, SC, SD, SE, SF)
+    )
 
-#     return
+
+def is_valid_ffn(ticket: str) -> bool:
+    """
+    The parameter takes a ticket 'ticket. The function should return True 
+    if and only if the frequent flyer number (FFN) of this ticket is valid. For
+    a ticket to be valid FFN, it must contain exactly four digits, and the 
+    sum of the first three digits modulo 10 must be the same as the last 
+    (fourth) digit. If an FFN doesn't exist, True is returned.
+    Precondition: 'ticket' is in valid format.
+
+    >>> is_valid_ffn('20230915YYZYEG12F1236')
+    True
+    >>> is_valid_ffn('20230915YYZYEG42F2468')
+    False
+    >>> is_valid_ffn('20230915YYZYEG42F')
+    True
+    """
+
+    ffn = get_ffn(ticket)
+    return (
+        ffn == "" or
+        ffn.isdigit() and
+        len(ffn) == 4 and
+        sum([int(num) for num in ffn[:3]]) % 10 == int(ffn[3])
+    )
+
+
+# months with 31 days
+MONTHS_WITH_31 = {1, 3, 5, 7, 8, 10, 12}
+
+
+def is_leap_year(yr: int) -> bool:
+    """
+    Utility function to check for a leap year in year 'yr'
+    Constraint: yr >= 0
+
+    >>> is_leap_year(1000)
+    False
+    >>> is_leap_year(1996)
+    True
+    >>> is_leap_year(4000)
+    True
+    >>> is_leap_year(2100)
+    False
+    """
+
+    return yr % 4 == 0 and (not yr % 100 == 0 or yr % 400 == 0)
+
+
+def is_valid_date(ticket: str) -> bool:
+    """
+    The parameter represents the ticket. The function should return
+    True if and only if the date of this ticket is valid.
+    Constraints: 
+    - Ticket must have be of form YYYYMMDD
+    - year YYYY, month MM, and day DD must be defined integers in 
+      string form
+
+    >>> is_valid_date('20230915YYZYEG12F1236')
+    True
+    >>> is_valid_date('20290931YYZYEG42F')
+    False
+    >>> is_valid_date('20000231YYZYEG42F')
+    False
+    >>> is_valid_date('20000229YYZYEG42F') # N00 leap year 29th
+    True
+    >>> is_valid_date('21040229YYZYEG42F') # regular leap year 29th
+    True
+    >>> is_valid_date('21000229YYZYEG42F') # N00 non leap year 29th
+    False
+    >>> is_valid_date('21000228YYZYEG42F') # N00 non leap year non 29th
+    True
+    """
+
+    year = int(get_year(ticket))
+    month = int(get_month(ticket))
+    day = int(get_day(ticket))
+
+    leap_year: bool = is_leap_year(year)
+    return (
+        1 <= month <= 12 and (
+            month in MONTHS_WITH_31 and 1 <= day <= 31 or
+            month == 2 and 1 <= day <= (28 + int(leap_year)) or
+            month != 2 and 1 <= day <= 30
+        )
+    )
+
+
+def is_valid_ticket(ticket: str, first_row: int, last_row: int) -> bool:
+    """
+    The first parameter takes the ticket, 'ticket'. The second 'first_row' 
+    parameter represents the number of the first row in the plane. The third 
+    parameter 'last_row' represents the number of the last row in the plane. 
+    The function should return True if and only if the ticket is in valid 
+    format and all of the ticket information on this ticket is valid.
+
+    >>>
+
+    """
+
+    return (
+        is_valid_ticket_format(ticket) and
+        is_valid_date(ticket) and
+        is_valid_seat(first_row, last_row) and
+        is_valid_ffn(ticket)
+    )
+
+
+# We provide this function for you to use as a helper.
+def is_valid_ticket_format(ticket: str) -> bool:
+    """Return True if and only if ticket 'ticket' is in valid format:
+
+    - year is 4 digits
+    - months is 2 digits
+    - day is 2 digits
+    - departure is 3 letters
+    - arrival is 3 letters
+    - row is 2 digits
+    - seat is a characters
+    - frequent flyer number is either empty or 4 digits, and
+      it is the last record in 'ticket'
+
+    >>> is_valid_ticket_format('20241020YYZYEG12C1236')
+    True
+    >>> is_valid_ticket_format('20241020YYZYEG12C12361236')
+    False
+    >>> is_valid_ticket_format('ABC41020YYZYEG12C1236')
+    False
+    """
+    return (FFN == 17
+            and (len(ticket) == 17
+                 or len(ticket) == 21 and ticket[FFN:FFN + 4].isdigit())
+            and ticket[YR:YR + 4].isdigit()
+            and ticket[MON:MON + 2].isdigit()
+            and ticket[DAY:DAY + 2].isdigit()
+            and ticket[DEP:DEP + 3].isalpha()
+            and ticket[ARR:ARR + 3].isalpha()
+            and ticket[ROW:ROW + 2].isdigit()
+            and ticket[SEAT].isalpha())
 
 
 """
@@ -249,38 +377,6 @@ def visits_airport(ticket: str, airport: str) -> bool:
 
 #     pass  # replace this line with your solution
 
-
-# # We provide this function for you to use as a helper.
-# def is_valid_ticket_format(ticket: str) -> bool:
-#     """Return True if and only if ticket 'ticket' is in valid format:
-
-#     - year is 4 digits
-#     - months is 2 digits
-#     - day is 2 digits
-#     - departure is 3 letters
-#     - arrival is 3 letters
-#     - row is 2 digits
-#     - seat is a characters
-#     - frequent flyer number is either empty or 4 digits, and
-#       it is the last record in 'ticket'
-
-#     >>> is_valid_ticket_format('20241020YYZYEG12C1236')
-#     True
-#     >>> is_valid_ticket_format('20241020YYZYEG12C12361236')
-#     False
-#     >>> is_valid_ticket_format('ABC41020YYZYEG12C1236')
-#     False
-#     """
-#     return (FFN == 17
-#             and (len(ticket) == 17
-#                  or len(ticket) == 21 and ticket[FFN:FFN + 4].isdigit())
-#             and ticket[YR:YR + 4].isdigit()
-#             and ticket[MON:MON + 2].isdigit()
-#             and ticket[DAY:DAY + 2].isdigit()
-#             and ticket[DEP:DEP + 3].isalpha()
-#             and ticket[ARR:ARR + 3].isalpha()
-#             and ticket[ROW:ROW + 2].isdigit()
-#             and ticket[SEAT].isalpha())
 
 if __name__ == '__main__':
     import doctest
