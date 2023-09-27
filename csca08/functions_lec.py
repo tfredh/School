088e281e9344682cbbb1a9da5dfbcd0068e7d319
Frederic Tu <fredhtu@gmail.com>
2023-09-27 09:11:28 -0400

    comment changes

--- csca08/functions_lec.py
+++ csca08/functions_lec.py
@@ -84,10 +84,10 @@ def calculate_total(bill: float, tax_rate: float) -> float:
     """
     Return the total cost of the transaction accrued from the bill, `bill`,
     including a `tax_rate` tax rate on the bill, rounded to two decimal 
-    places.
+    places. Resulted rounded.
     Constraints:
     - bill >= 0
-    - tax_rate >= 0
+    - 0 <= tax_rate <= 1
 
     >>> calculate_total(50.0, 0.10)
     55.0
@@ -96,6 +96,54 @@ def calculate_total(bill: float, tax_rate: float) -> float:
     """
 
     return round(bill * (1 + tax_rate), 2)
+
+
+#
+#
+#
+
+"""Code for week 2 worksheets: sample solution.
+"""
+
+
+def format_name(first: str, last: str) -> str:
+    """Return the full name of a person with first name first_name and
+    last name last_name.  Specifically, return the string in the
+    format:
+    last_name, first_name
+    i.e. last_name followed by comma, followed by space, followed by
+    first_name.
+
+    >>> format_name('Anya', 'Tafliovich')
+    'Tafliovich, Anya'
+    >>> format_name('Ada', 'Lovelace')
+    'Lovelace, Ada'
+    """
+
+    return last + ', ' + first
+
+
+def to_listing(first: str, last: str, phone_number: str) -> str:
+    """Return the phone book listing of a person with first name first_name,
+    last name last_name, and phone number phone_number. Specifically,
+    return the string in the format:
+    last_name, first_name: phone_number
+    i.e. last_name followed by comma, followed by space, followed by
+    first_name followed by colon, followed by space, followed by
+    phone_number.
+
+    >>> to_listing('Anya', 'Tafliovich', '123-456-7890')
+    'Tafliovich, Anya: 123-456-7890'
+    >>> to_listing('Ada', 'Lovelace', '010')
+    'Lovelace, Ada: 010'
+    """
+
+    return format_name(first, last) + ': ' + phone_number
+
+
+# Homework: trace the line
+# listing = to_listing('Ada', 'Lovelace', '010')
+# in the visualizer!
 
 
 if __name__ == '__main__':
