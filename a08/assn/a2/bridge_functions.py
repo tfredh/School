b942cadd745f75097210c215142a474efc448706
gosqib <69024184+gosqib@users.noreply.github.com>
2023-10-31 20:34:10 -0400

    s

--- a08/assn/a2/bridge_functions.py
+++ a08/assn/a2/bridge_functions.py
@@ -318,6 +318,36 @@ def get_bridges_with_bci_below(
     return bridges_bci_below
 
 
+def get_bridges_containing(bridge_data: list[list], search: str) -> list[int]:
+    """
+    Returns a list of ids of all bridges in `bridge_data` whose names 
+    contain the search string, `search`. The search should be case-insensitive.
+
+    When called with the example THREE_BRIDGES and the search string 'underpass', 
+    the function should return the list [1, 2]. Note that when called with the 
+    example THREE_BRIDGES and the search string 'pass', the function should also 
+    return the list [1, 2], i.e. the function looks for part of the name, not 
+    necessarily the entire name.
+
+    >>> get_bridges_containing(THREE_BRIDGES, 'underpass')
+    [1, 2]
+    >>> get_bridges_containing(THREE_BRIDGES, 'pass')
+    [1, 2]
+    >>> get_bridges_containing(THREE_BRIDGES, 'asd')
+    []
+    >>> get_bridges_containing(THREE_BRIDGES, 'brid')
+    [3]
+    """
+
+    containing_search = []
+
+    for bridge in bridge_data:
+        if search.lower() in bridge[NAME_INDEX].lower():
+            containing_search.append(bridge[ID_INDEX])
+
+    return containing_search
+
+
 # We provide the header and doctring for this function to help get you
 # started. Note the use of the built-in function deepcopy (see
 # help(deepcopy)!): since this function modifies its input, we do not
@@ -388,6 +418,42 @@ def inspect_bridges(bridge_data: list[list], bridge_ids: list[int], date: str,
 
         bridge[LAST_INSPECTED_INDEX] = date
         bridge[BCIS_INDEX].insert(0, bci)
+
+
+def add_rehab(bridge_data: list[list], bridge_id: int, date: str,
+              major_rehab: bool) -> None:
+    """
+    Updates the bridges in `bridge_data` with the given ID, `bridge_id` with 
+    the new rehab year record: year of major rehab, if the last argument is 
+    True, and year or minor rehab if the last argument is False. If there is 
+    no bridge with the given id in the given bridge data, the function has no 
+    effect.
+
+    >>> bridges = deepcopy(THREE_BRIDGES)
+    >>> add_rehab(bridges, 1, '09/15/2023', False)
+    >>> bridges == [
+    ...    [1, 'Highway 24 Underpass at Highway 403', '403', 43.167233, -80.275567,
+    ...    '1965', '2014', '2023', 4, [12.0, 19.0, 21.0, 12.0], 65, '04/13/2012',
+    ...    [72.3, 69.5, 70.0, 70.3, 70.5, 70.7, 72.9]],
+    ...    [2, 'WEST STREET UNDERPASS', '403', 43.164531, -80.251582,
+    ...    '1963', '2014', '2007', 4, [12.2, 18.0, 18.0, 12.2], 61.0, '04/13/2012',
+    ...    [71.5, 68.1, 69.0, 69.4, 69.4, 70.3, 73.3]],
+    ...    [3, 'STOKES RIVER BRIDGE', '6', 45.036739, -81.33579,
+    ...    '1958', '2013', '', 1, [16.0], 18.4, '08/28/2013',
+    ...    [85.1, 67.8, 67.4, 69.2, 70.0, 70.5, 75.1, 90.1]]
+    ... ]
+    True
+    """
+
+    for bridge in bridge_data:
+        if not bridge[ID_INDEX] == bridge_id:
+            continue
+
+        bridge[
+            LAST_MAJOR_INDEX
+            if major_rehab
+            else LAST_MINOR_INDEX
+        ] = date[-4:]
 
 
 # # We provide the header and doctring for this function to help get you started.
