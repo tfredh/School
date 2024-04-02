ed845a28a1b79d1ad42e7a9e118d429b48f8bd72
gosqib <69024184+gosqib@users.noreply.github.com>
2024-04-01 22:47:20 -0400

    dumb

---
+++ a48/exer/ex12/ex12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("The exercises were helpful in reviewing the content of the week "
+           "and were nice small tasks I could do to practice active recall "
+           "based learning. I wish they included tasks that would force you to "
+           "use some more niche concepts of the language though since I didn't "
+           "expect so many niche details about C on the midterm. I expected "
+           "the exam would focus on common C topics and data structures and "
+           "algorithms as the course description said. Instead, the multiple "
+           "choice had a lot of C trivia questions that made it feel like a "
+           "test of the language C itself.\n");
+    printf(
+        "For summer, firstly, I hope I'm not retaking A48 for cs post. "
+        "Besides that tho, I'm going to take summer off of school besides a "
+        "few elective courses to fulfill breadth requirements and get a "
+        "credit out the way, then the rest of the time relaxing with friends "
+        "or at a recreation center, get some miscellaneous personal tasks "
+        "done, and some software development work to help build my resume.\n");
+}
